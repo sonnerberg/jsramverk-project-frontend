786fe067eab62106be38b4e96e8a79a1703721c9
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-10-22 21:06:02 +0200

    Add linting
    Squashed commit of the following:
    
    commit 4910fdc7bfb779f4577c7cbff4ccf24910c551a0
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Thu Oct 22 21:05:47 2020 +0200
    
        Add lint and fix npm script
    
    commit b7a0e141cc048ce2e611f6d8c6580b255c1ec88a
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Thu Oct 22 21:04:29 2020 +0200
    
        Lint and fix
    
    commit 45d74b7a1830435f31474a3e6890bad8279cd6d0
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Thu Oct 22 21:04:12 2020 +0200
    
        Add prettier configuration
    
    commit 2a4cbac3cfd8b2f1e201f4e76845083a299627c1
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Thu Oct 22 21:04:05 2020 +0200
    
        Add eslint configuration
    
    commit 04f79f5989be53bf7f31e73b236458f5f33962ad
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Thu Oct 22 21:03:56 2020 +0200
    
        Add dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'prettier', 'jsx-a11y'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 1,
+    'react/prop-types': 1,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
