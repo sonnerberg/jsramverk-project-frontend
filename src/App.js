8a1088a3635a89d725b2e3e0c9d9ca8fd3fdac2d
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-10-23 09:44:22 +0200

    Add graphQL
    Squashed commit of the following:
    
    commit fcb82716450a469e168f48ad15152d16c0e3d32d
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Fri Oct 23 09:44:01 2020 +0200
    
        Add mutation file
    
    commit 3f99b16de2be2554cd56fb76b45b3290a293798d
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Fri Oct 23 09:43:52 2020 +0200
    
        Add example query
    
    commit 33e475e5b1a113a30188fe51ec115ce1305dceac
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Fri Oct 23 09:43:32 2020 +0200
    
        Show information about users
    
    commit da60e6037b4bc832e536f20bcae09350381899b8
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Fri Oct 23 09:43:13 2020 +0200
    
        Use ApolloProvider
    
    commit e5682c93260395e4b5a518b263a0790ba7cbcac9
    Author: Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
    Date:   Fri Oct 23 09:42:48 2020 +0200
    
        Add dependencies

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import GlobalStyle from './components/GlobalStyle'
 import './App.css'
 
 import Pages from './pages'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Pages />
-    </>
-  )
-}
+const uri = process.env.REACT_APP_API_URI
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+})
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <GlobalStyle />
+    <Pages />
+  </ApolloProvider>
+)
 
 export default App
--- src/pages/users.js
+++ src/pages/users.js
@@ -1,9 +1,50 @@
+import { gql, useQuery } from '@apollo/client'
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+const GET_USERS = gql`
+  query get_users {
+    users {
+      username
+      email
+      avatar
+    }
+  }
+`
 
 const Users = () => {
+  const { data, loading, error } = useQuery(GET_USERS)
+
+  if (loading)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    )
+
+  if (error)
+    return (
+      <>
+        <p>Error!</p>
+      </>
+    )
+
   return (
     <>
       <div>This is where you can see statistics of registered users</div>
+      {data.users.map((user) => (
+        <React.Fragment key={uuidv4()}>
+          <div>
+            <img
+              src={user.avatar}
+              alt={`${user.username} avatar`}
+              height="50px"
+            />
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        </React.Fragment>
+      ))}
     </>
   )
 }
